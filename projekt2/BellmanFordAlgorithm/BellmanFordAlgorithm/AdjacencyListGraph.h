9d6444e94f36dd9bebed460b2a71b0a96e587faf
unknown <karolina.gluszek6@gmail.com>
2020-04-13 23:47:05 +0200

    Refactor klasy grafu jako listy sąsiedztwa

--- projekt2/BellmanFordAlgorithm/BellmanFordAlgorithm/AdjacencyListGraph.h
+++ projekt2/BellmanFordAlgorithm/BellmanFordAlgorithm/AdjacencyListGraph.h
@@ -2,46 +2,92 @@
 #include <iostream>
 #include "LinkedList.h"
 
+struct Vertex
+{
+	int index;
+};
+
+struct Edge
+{
+	Vertex* fromIndex;
+	Vertex* toIndex;
+	int weight;
+};
+
 class AdjancencyListGraph
 {
-private:
-	struct Neighbour
-	{
-		int vertexIndex;
-
-		int weight;
-	};
-
 public:
-	LinkedList<Neighbour>* verticesArray;
+	LinkedList<Edge*>* adjancencyList;
+	LinkedList<Edge*> edgesList;
+	Vertex** verticesArray;
+	int numberOfVertices;
+	int numberOfEdges;
 
-	int edges;
+	AdjancencyListGraph(int verticesNumber, int edgesNumber) :
+		numberOfVertices(verticesNumber),
+		numberOfEdges(edgesNumber),
+		edgesList(LinkedList<Edge*>()),
+		verticesArray(new Vertex*[numberOfVertices]),
+		adjancencyList(new LinkedList<Edge*>[numberOfVertices]){}
 
-	int vertices;
-
-	AdjancencyListGraph(int edg, int ver) : edges(edg), vertices(ver)
+	~AdjancencyListGraph()
 	{
-		verticesArray = new LinkedList<Neighbour>[vertices];
+		removeVertices();
+		removeEdges();	
 	}
-	
-	void addEdge(int fromIndex, int toIndex, int weight)
+
+	void createVertex(int vertexIndex)
 	{
-		verticesArray[fromIndex].addFront({ toIndex, weight });
+		Vertex* vertex = new Vertex();
+		vertex->index = vertexIndex;
+		verticesArray[vertexIndex] = vertex;
+		std::cout << "create: " << vertex << "\n";
 	}
-	
-	void printList()
+
+	void createEdge(int from, int to, int weight)
 	{
-		for (int i = 0; i < vertices; i++)
+		Edge* edge = new Edge();
+		edge->fromIndex = verticesArray[from];
+		edge->toIndex = verticesArray[to];
+		edge->weight = weight;
+		edgesList.addFront(edge);
+		adjancencyList[from].addFront(edge);
+	}
+
+	void removeVertices()
+	{
+		for (int i = 0; i < numberOfVertices; i++)
 		{
-			std::cout << i << " ";
-			for (Neighbour listElement : verticesArray[i])
-				std::cout << "[" << listElement.vertexIndex << ", " << listElement.weight << "]";
-			std::cout << "\n";
+			std::cout << "remove: " << verticesArray[i] << "\n";
+			delete verticesArray[i];		
+		}
+		delete[] verticesArray;
+	}
+	
+	void removeEdges()
+	{
+		for (int i = 0; i < numberOfEdges; i++)
+		{
+			delete edgesList.front();
+			edgesList.removeFront();
 		}
 	}
 	
-	~AdjancencyListGraph()
+	LinkedList<Edge*> incidentEdges(Vertex* vertex)
 	{
-		delete[] verticesArray;
+		return adjancencyList[vertex->index];
+	}
+	
+	LinkedList<Vertex*> vertices()
+	{
+		LinkedList<Vertex*> list;
+		for (int i = 0; i < numberOfVertices; i++)
+			list.addFront(verticesArray[i]);
+		return list;
+	}
+
+	LinkedList<Edge*> edges()
+	{
+		return edgesList;
 	}
 };
